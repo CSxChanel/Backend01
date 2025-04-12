7c5a3a4938e635a5c6cb5b37b82995017d4c2610
Cp_Sudrajat <120399183+CSxChanel@users.noreply.github.com>
2025-04-13 01:23:06 +0700

    Update app.js merubah port 8080 railway

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware global
 app.use(cors({ origin: true, credentials: true })); // untuk tes API dari luar semua domain
